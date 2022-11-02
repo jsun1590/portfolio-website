f3bbbbda95e426b519e5df9eeedec652688e99bb
Jack Sun <jsun1590@gmail.com>
2022-11-02 13:55:41 +0800

    add email and keybase

--- components/Sections/Footer/Social.tsx
+++ components/Sections/Footer/Social.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-
+import {
+  faGithub,
+  faLinkedin,
+  faKeybase,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 const Social = () => {
   return (
     <section className="flex justify-center py-4">
       <a
-        href=""
+        href="https://github.com/jsun1590/"
         className="mx-2 rounded-[50%] shadow-circle dark:shadow-white hover:!shadow-yellow-300 transition-all hover:-translate-y-1"
       >
         <FontAwesomeIcon
@@ -16,12 +20,32 @@ const Social = () => {
       </a>
 
       <a
-        href=""
+        href="https://www.linkedin.com/in/jack-sun-a743b6220/"
         className="mx-2 rounded-[50%] shadow-circle dark:shadow-white hover:!shadow-blue-500 transition-all hover:-translate-y-1"
       >
         <FontAwesomeIcon
           icon={faLinkedin}
           className="transition-colors w-10 h-10 p-2 text-center dark:text-white hover:!text-blue-500"
+        />
+      </a>
+
+      <a
+        href="mailto:jsun1590@gmail.com"
+        className="mx-2 rounded-[50%] shadow-circle dark:shadow-white hover:!shadow-emerald-400 transition-all hover:-translate-y-1"
+      >
+        <FontAwesomeIcon
+          icon={faEnvelope}
+          className="transition-colors w-10 h-10 p-2 text-center dark:text-white hover:!text-emerald-400"
+        />
+      </a>
+
+      <a
+        href="https://keybase.io/jsun1590"
+        className="mx-2 rounded-[50%] shadow-circle dark:shadow-white hover:!shadow-orange-500 transition-all hover:-translate-y-1"
+      >
+        <FontAwesomeIcon
+          icon={faKeybase}
+          className="transition-colors w-10 h-10 p-2 text-center dark:text-white hover:!text-orange-500"
         />
       </a>
     </section>
