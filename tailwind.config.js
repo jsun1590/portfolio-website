8ba1cf48b1b8f18aab4a6f1448122b93e0c39b0c
Jack Sun <jsun1590@gmail.com>
2022-11-03 14:53:30 +0800

    add nav menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,50 @@
 module.exports = {
   darkMode: "class",
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#5c6ac4'
+        primary: "#5c6ac4",
       },
 
       boxShadow: {
-        'circle': '0 0 0 1px'
+        circle: "0 0 0 1px",
       },
 
       animation: {
-        'waving': 'waving 1s'
+        waving: "waving 1s",
       },
 
       keyframes: {
-        'waving': {
-          '0%': { transform: 'rotate( 0.0deg)' },
-          '20%': { transform: 'rotate(14.0deg)' },
-          '40%': { transform: 'rotate(-8.0deg)' },
-          '70%': { transform: 'rotate(10.0deg)' },
-          '100%': { transform: 'rotate( 0.0deg)' },
-        }
-
-      }
-
+        waving: {
+          "0%": { transform: "rotate( 0.0deg)" },
+          "20%": { transform: "rotate(14.0deg)" },
+          "40%": { transform: "rotate(-8.0deg)" },
+          "70%": { transform: "rotate(10.0deg)" },
+          "100%": { transform: "rotate( 0.0deg)" },
+        },
+      },
     },
   },
   plugins: [
-    function ({ addVariant }) {
-        addVariant('child', '& > *');
-        addVariant('child-hover', '& > *:hover');
-        addVariant('child-2', '& > * > *');
-        addVariant('child-2-hover', '& > *:hover');
-    }
-],
-}
+    function ({ addVariant, matchUtilities, addUtilities, theme }) {
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+      addVariant("child-2", "& > * > *");
+      addVariant("child-2-hover", "& > *:hover");
+      matchUtilities(
+        {
+          "translate-z": (value) => ({
+            "--tw-translate-z": value,
+            transform: ` translate3d(var(--tw-translate-x), var(--tw-translate-y), var(--tw-translate-z)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))`,
+          }),
+        },
+        { values: theme("translate"), supportsNegativeValues: true }
+      );
+      addUtilities({ ".transform-3d": { transformStyle: "preserve-3d" } });
+    },
+  ],
+};
